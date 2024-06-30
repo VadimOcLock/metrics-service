7cc2d02a787a7378875d532c74f1b07a6feca6fc
VadimOcLock <vadimoclock@gmail.com>
2024-06-30 23:55:00 +0300

    chi router

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/VadimOcLock/metrics-service
 
 go 1.22.3
 
-require github.com/safeblock-dev/wr v0.1.0
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/safeblock-dev/wr v0.1.0
+)
 
 require github.com/safeblock-dev/werr v0.0.8 // indirect
--- internal/api/handler/handler.go
+++ internal/api/handler/handler.go
@@ -5,18 +5,20 @@ import (
 	"github.com/VadimOcLock/metrics-service/internal/service/metric_service"
 	"github.com/VadimOcLock/metrics-service/internal/store/some_store"
 	"github.com/VadimOcLock/metrics-service/internal/usecase/metric_usecase"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
 func New(ctx context.Context) http.Handler {
-	mux := http.NewServeMux()
+	// Использовал chi так как не нашел способа распарсить пустые сегменты без регулярок
+	r := chi.NewRouter()
 
 	store := some_store.New()
 	metricService := metric_service.New(&store)
 	metricUseCase := metric_usecase.New(metricService)
 	updateMetricsHandler := NewUpdateMetricsHandler(ctx, metricUseCase)
 
-	mux.Handle("/update/", updateMetricsHandler)
+	r.Post("/update/{type:[^/]*}/{name:[^/]*}/{value:[^/]*}", updateMetricsHandler.ServeHTTP)
 
-	return Chain(mux, LoggerMiddleware)
+	return Chain(r, LoggerMiddleware)
 }
