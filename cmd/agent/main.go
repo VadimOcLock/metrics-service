78c32d8fd3d420c3b255863e0abb93b572f8d866
VadimOcLock <vadimoclock@gmail.com>
2024-07-01 12:14:24 +0300

    agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"github.com/VadimOcLock/metrics-service/internal/worker"
+	"github.com/VadimOcLock/metrics-service/pkg/lifecycle"
+	"github.com/safeblock-dev/wr/taskgroup"
+	"os"
+	"syscall"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	srvAddr := "http://localhost:8080"
+
+	w := worker.NewMetricsWorker(worker.MetricsWorkerOpts{
+		ServerAddr:     srvAddr,
+		PoolInterval:   2 * time.Second,
+		ReportInterval: 10 * time.Second,
+	})
+
+	tasks := taskgroup.New()
+	tasks.Add(taskgroup.SignalHandler(ctx, os.Interrupt, syscall.SIGINT, syscall.SIGTERM))
+	tasks.Add(lifecycle.Worker(w))
+	_ = tasks.Run()
+}
